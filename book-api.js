8161a6eadea2087e05ba46cd12cc60a20633fa68
devenock <enockomondi305@gmail.com>
2020-11-11 18:06:53 -0500

    Working on the operations

--- book-api.js
+++ book-api.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('view engine', 'ejs');
+
 app.use(cors());
 
 // declare books array
@@ -24,7 +26,50 @@ app.post('/book', (req, res) => {
     books.push(book);
 
     res.send('Book is added to the database');
-})
+});
+
+// app.get('/books', (req, res) => {
+//     res.render('newbook');
+// })
+
+// getting all books
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+// get specific book
+app.get('/book/:isbn', (req, res) => {
+    // Reading isbn from the URL
+    const isbn = req.params.isbn;
+
+    // Searching books for the isbn
+    for (let book of books) {
+        if (book.isbn === isbn) {
+            res.json(book);
+            return;
+        }
+    }
+
+    // deleting the book
+    app.delete('/book/:isbn', (req, res) => {
+        // Reading isbn from the URL
+        const isbn = req.params.isbn;
+
+        // Remove item from the books array
+        books = books.filter(i => {
+            if (i.isbn !== isbn) {
+                return true;
+            }
+            return false;
+        });
+
+        res.send('Book is deleted');
+    });
+
+    // Sending 404 when not found something is a good practice
+    res.status(404).send('Book not found');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
