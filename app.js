3be45759873827228bae07302eacdfae9dd7e0e8
devenock <enockomondi305@gmail.com>
2020-11-11 05:36:46 -0500

    Adding the book view

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const ejs = require('ejs');
 
 const app = express();
 
@@ -11,12 +12,23 @@ let books = [];
 
 app.use(cors());
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Our Bookapi Application')
+    res.render('book');
+});
+
+// creating a new book
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    console.log(book);
+    books.push(book);
+    res.send('Book added to the database');
 });
 
 app.listen(port, () => {
